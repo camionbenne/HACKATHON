9b7297b2cf8b5a47a5929bb4881ab99e220c9a2f
Mattéo <matteo.stirn@gmail.com>
2025-12-03 11:45:18 +0100

    Selection Melun opérationnel

--- get_tempé_campagne.py
+++ get_tempé_campagne.py
@@ -10,16 +10,26 @@ import os
 
 ### Chemins des données
 parametre = ["tasAdjust", "tasmaxAdjust", "tasminAdjust"]
-periode = ["20150101-20191231","20200101-20291231","20300101-20391231","20400101-20491231","20500101-20591231","20600101-20691231","20700101-20791231","20800101-20891231","20900101-20991231"]
+periode = ["20150101-20191231","20200101-20291231","20300101-20391231","20400101-20491231",
+           "20500101-20591231","20600101-20691231","20700101-20791231","20800101-20891231",
+           "20900101-20991231"]
 
-i_param = 0 #indice du parametre choisi
-i_periode = 0 #indice pour la periode
+i_param = 0
+i_periode = 0
+
 chemin_file = f"../CPRCM/ssp370/{parametre[i_param]}/"
-nom_fichier = f"{parametre[i_param]}_FR-Metro_CNRM-ESM2-1_ssp370_r1i1p1f2_CNRM-MF_CNRM-AROME46t1_v1-r1_MF-CDFt-ANASTASIA-ALPX-3-1991-2020_day_{periode[i_periode]}.nc"
+nom_fichier = (
+    f"{parametre[i_param]}_FR-Metro_CNRM-ESM2-1_ssp370_r1i1p1f2_"
+    f"CNRM-MF_CNRM-AROME46t1_v1-r1_MF-CDFt-ANASTASIA-ALPX-3-1991-2020_day_"
+    f"{periode[i_periode]}.nc"
+)
 
 T_data = xr.open_dataset(chemin_file + nom_fichier)
 
 
+###########################################
+#   Masque rectangle SANS sel() (robuste)
+###########################################
 def mask_rectangle(data, lat_min, lat_max, lon_min, lon_max):
     """
     Applique un masque géographique à un DataArray.
@@ -27,57 +37,73 @@ def mask_rectangle(data, lat_min, lat_max, lon_min, lon_max):
     """
     lats = data['lat']
     lons = data['lon']
-    mask_bool = (lats >= lat_min) & (lats <= lat_max) & (lons >= lon_min) & (lons <= lon_max)
-    
-    # where garde les coordonnées et remplace les valeurs hors masque par NaN
-    return data.where(mask_bool)
 
+    # Construction du masque 2D correct via broadcasting
+    mask = (
+        (lats >= lat_min) & (lats <= lat_max)
+    ) & (
+        (lons >= lon_min) & (lons <= lon_max)
+    )
+
+    return data.where(mask, drop = True)
+
+
+###########################################
+#   Extraction Melun
+###########################################
 def get_melun_tempe(data):
-    """
-    Donne la tempé de Melun
-    data : xarray.DataArray
-    """
     lat_Melun = 48.572551
     lon_Melun = 2.672507
-    data_Melun = data.sel(lat=lat_Melun, lon=lon_Melun, method="nearest")
 
-    return data_Melun
+    lats = data['lat'].values   
+    lons = data['lon'].values
+
+    # distance brute
+    dist = np.sqrt((lats - lat_Melun)**2 + (lons - lon_Melun)**2)
+
+    # indices du point le plus proche
+    j, i = np.unravel_index(np.argmin(dist), dist.shape)
+
+    return data[:, j, i]
 
 
 
-
-# Zone Île-de-France
+###########################################
+#   Zone géographique
+###########################################
 lat_min, lat_max = 48.0, 49.2
 lon_min, lon_max = 1.8, 3.6
 
-T_data_IDF = mask_rectangle(T_data[f"{parametre[i_param]}"], lat_min, lat_max, lon_min, lon_max)
-T_data_Melun = get_melun_tempe(T_data_IDF)
+T_data_IDF = mask_rectangle(T_data[parametre[i_param]], lat_min, lat_max, lon_min, lon_max)
+
+T_data_Melun = get_melun_tempe(T_data[parametre[i_param]])
 
 
-# Moyenne temporelle sur la zone masquée
-moy_data = T_data_IDF.mean(dim='time', skipna=True).values - 273.15
-moy_T_melun = T_data_Melun.mean(dim='time', skipna=True).values - 273.15
+###########################################
+#   Moyennes temporelles
+###########################################
+moy_data = T_data_IDF.mean(dim="time", skipna=True).values - 273.15
+moy_T_melun = T_data_Melun.mean(dim="time", skipna=True).values - 273.15
 
+anomalie_par_melun = moy_data - moy_T_melun
 
-
-
-
-
-
-
-
-###################################################################
-#######################  AFFICHAGE  ###############################
-###################################################################
+###########################################
+#   AFFICHAGE
+###########################################
 lats_data = T_data_IDF['lat'].values
 lons_data = T_data_IDF['lon'].values
 
 lat_lim_idf = [lat_min, lat_max]
 lon_lim_idf = [lon_min, lon_max]
 
-# Création figure
-fig, ax = plt.subplots(1, 1, figsize=(8, 6), subplot_kw={'projection': ccrs.LambertConformal(central_longitude=3, central_latitude=48.6)})
+# Création figure avec deux sous-graphes
+fig, axs = plt.subplots(
+    1, 2, figsize=(16, 6),
+    subplot_kw={'projection': ccrs.LambertConformal(central_longitude=3, central_latitude=48.6)}
+)
 
+# --- Température moyenne ---
+ax = axs[0]
 ax.set_extent([lon_lim_idf[0], lon_lim_idf[1], lat_lim_idf[0], lat_lim_idf[1]], crs=ccrs.PlateCarree())
 ax.coastlines('10m', color='black', linewidth=0.8)
 
@@ -86,27 +112,48 @@ gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True, linestyle='--', alph
 gl.top_labels = False
 gl.right_labels = False
 gl.xformatter = LONGITUDE_FORMATTER
-gl.yformatter = LATITUDE_FORMATTER
+gl.yformatter = LONGITUDE_FORMATTER
 gl.xlocator = mticker.FixedLocator(np.arange(lon_lim_idf[0], lon_lim_idf[1]+0.1, 0.2))
 gl.ylocator = mticker.FixedLocator(np.arange(lat_lim_idf[0], lat_lim_idf[1]+0.1, 0.2))
-gl.xlabel_style = {"size": 10, "color": "gray"}
-gl.ylabel_style = {"size": 10, "color": "gray"}
 
-# Tracé lisse
-mm = ax.pcolormesh(lons_data, lats_data, moy_data-moy_T_melun,
-                   vmin=np.nanmin(moy_data), vmax=np.nanmax(moy_data),
-                   cmap=mpl.cm.viridis, shading='auto',
-                   transform=ccrs.PlateCarree())
+mm0 = ax.pcolormesh(
+    lons_data, lats_data, moy_data,
+    vmin=np.nanmin(moy_data), vmax=np.nanmax(moy_data),
+    cmap=mpl.cm.viridis, shading='auto',
+    transform=ccrs.PlateCarree()
+)
+cbar0 = plt.colorbar(mm0, ax=ax, orientation='vertical', shrink=0.7)
+cbar0.set_label('Température moyenne (°C)', fontsize=12)
+ax.set_title("Température moyenne 2015–2019", fontsize=14)
 
-# Colorbar
-cbar = plt.colorbar(mm, orientation='vertical', shrink=0.7)
-cbar.set_label('Température moyenne (°C)', fontsize=12)
+# --- Anomalie par rapport à Melun ---
+ax = axs[1]
+ax.set_extent([lon_lim_idf[0], lon_lim_idf[1], lat_lim_idf[0], lat_lim_idf[1]], crs=ccrs.PlateCarree())
+ax.coastlines('10m', color='black', linewidth=0.8)
 
-plt.title("Température moyenne 2015-2019 - Île-de-France", fontsize=14)
+# Grille
+gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True, linestyle='--', alpha=0.3)
+gl.top_labels = False
+gl.right_labels = False
+gl.xformatter = LONGITUDE_FORMATTER
+gl.yformatter = LONGITUDE_FORMATTER
+gl.xlocator = mticker.FixedLocator(np.arange(lon_lim_idf[0], lon_lim_idf[1]+0.1, 0.2))
+gl.ylocator = mticker.FixedLocator(np.arange(lat_lim_idf[0], lat_lim_idf[1]+0.1, 0.2))
+
+
+mm1 = ax.pcolormesh(
+    lons_data, lats_data, anomalie_par_melun,
+    vmin=-np.nanmax(abs(anomalie_par_melun)),
+    vmax=np.nanmax(abs(anomalie_par_melun)),
+    cmap='coolwarm', shading='auto',
+    transform=ccrs.PlateCarree()
+)
+cbar1 = plt.colorbar(mm1, ax=ax, orientation='vertical', shrink=0.7)
+cbar1.set_label('Anomalie par rapport à Melun (°C)', fontsize=12)
+ax.set_title("Anomalie par rapport à Melun 2015–2019", fontsize=14)
 
 # Sauvegarde
-chemin_fig = 'Cartes/'
+chemin_fig = "Cartes/"
 os.makedirs(chemin_fig, exist_ok=True)
-figname = chemin_fig + f'carte_tasAdjust_IDF_2015_2019.png'
-plt.savefig(figname, dpi=200)
+plt.savefig(chemin_fig + "carte_tasAdjust_IDF_2015_2019_double.png", dpi=200)
 plt.close()
